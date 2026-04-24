ecda0dc595faed2b604c885fef3572294e9e3307
Dragos George Stan <33067050+dragoswolf@users.noreply.github.com>
2026-04-24 19:21:25 +0200

    Comentarios mejorados
    Mejorado los comentarios para que sean más explicativos.

--- mod_1/check_mod1.py
+++ mod_1/check_mod1.py
@@ -21,56 +21,107 @@
 #=========================================================================================================
 
 
-import os
-import sys
-import subprocess
-import getpass
-import logging
+import os           #Comprueba existencia de ficheros y permisos
+import sys          #Para códigos de salida
+import subprocess   #Ejecuta comandos de verificación
+import getpass      #Para solicitar información sensible de forma segura
+import logging      #Para logging de los comandos usados y el resultado de la ejecución
 from datetime import datetime
 
 
-#Ficheros de conf y directorios importantes
+#=========================================================================================================
+# CONSTANTES GLOBALES - Ruta de los ficheros a verificar
+#=========================================================================================================
+
+#Fichero de configuración personalizada de GRUB (para el paso 1)
 GRUB_CUSTOM_FILE="/etc/grub.d/40_custom"
+
+#Fichero de configuración compilada de GRUB (generado por update-grub en el paso 1)
 GRUB_CFG_FILE= "/boot/grub/grub.cfg"
+
+#Fichero modprobe para el bloqueo de USB (paso 3)
 USB_MODPROBE_FILE="/etc/modprobe.d/usb-storage.conf"
+
+#Directorio y fichero de logs
 LOG_DIR="/var/log/hardening"
 LOG_FILE="/var/log/hardening/modulo1_fix.log"
 
-#variables extra para chequeo
-totalChecks=0
-checksOk=0
-checksFail=0
-checksWarn=0
-
-
-#FUNCIONES DE APOYO
 #=========================================================================================================
-#Función para la configuración logging
+
+
+#=========================================================================================================
+# CONTADORES GLOBALES - Variables globales usadas para el resumen final
+#=========================================================================================================
+
+# Estas variables llevan la cuenta de verificaciones correctas y fallidas
+totalChecks=0       #Número total de verificaciones realizadas
+checksOk=0          #Número de verificaciones correctas
+checksFail=0        #Número de verificaciones fallidas
+checksWarn=0        #Número de advertencias
+
+#=========================================================================================================
+
+#=========================================================================================================
+# FUNCIONES DE APOYO - Funciones utilizadas para realizar las funciones necesarias
+#=========================================================================================================
+
+# CONFIGURACIÓN DEL SISTEMA DE LOGS
+
 def configurar_logging():
+    """
+    Configura el sistema de logging para registrar errores en un fichero.
+    Crea el directorio /var/log/hardening/ si no existe.
+    El fichero de log es /var/log/hardening/modulo1_check.log
+    Cada entrada incluyte fecha, hora, nivel y mensaje.
+    """
+    #Crea el directorio de logs si no existe
     if not os.path.isdir(LOG_DIR):
         os.makedirs(LOG_DIR, exist_ok=True)
 
-    #formato de logging
+    #Configura el logger con el formato "[YYYY-MM-DD HH:MM:SS] ERROR: <mensajeError>"
     logging.basicConfig(filename=LOG_FILE, 
                         level=logging.ERROR, 
                         format="[%(asctime)s] %(levelname)s: %(message)s", 
                         datefmt="%Y-%m-%d %H:%M:%S"
                         )
     
-#función para registrar errores en los logs
 def registrar_errores(paso, mensaje):
+    """
+    Registra un error en el fichero de log y además lo muestra por pantalla
+
+    Args:
+        paso (str): Identificador del paso donde ocurrió el error (ej: "Paso 1").
+        mensaje (str): Descripción del error.
+    Return:
+        None.
+    """
     textoLog=f"[{paso}] {mensaje}"
     logging.error(textoLog)
     print(f"[ERROR]: {mensaje}")
 
-#función para comprobar el uso de sudo
+# ASEGURAR USO DE SUDO
+
 def comprobar_root():
+    """
+    Comprueba que el script se ejecuta como root.
+    Necesario para leer ficheros protegidos (como /boot/grub/grub.cfg)
+    """
     if os.geteuid()!=0:
         print("[ERROR]: Este script ha de ejecutarse como root.")
         print("         Ejecuta: sudo python3 fix_mod1.py")
         sys.exit(1)
 
+# RECOGIDA DE RESULTADOS
 def resultado_fail(mensaje, paso="General"):
+    """
+    Registra una verificación fallida y muestra el resultado en rojo.
+    
+    Args:
+        mensaje (str): Descripción de lo que ha fallado
+        paso (str): Identificador del paso para el log
+    Return:
+        None.
+    """
     global totalChecks, checksFail
     totalChecks +=1
     checksFail +=1
@@ -80,6 +131,14 @@ def resultado_fail(mensaje, paso="General"):
     registrar_errores(paso, mensaje)
 
 def resultado_warn(mensaje):
+    """
+    Registra una advertencia y muestra el resultado en amarillo.
+
+    Args:
+        mensaje (str): Descripción de la advertencia
+    Return:
+        None.
+    """
     global totalChecks, checksWarn
     totalChecks+=1
     checksWarn+=1
@@ -87,6 +146,14 @@ def resultado_warn(mensaje):
     print(f"    \033[93m[AVISO]:\033[0m {mensaje}")
 
 def resultado_ok(mensaje):
+    """
+    Registra una verificación exitosa y muestra el resultado en verde.
+
+    Args:
+        mensaje(str): Descripción de lo que se ha verificado correctamente.
+    Return:
+        None.
+    """
     global totalChecks, checksOk
     totalChecks+=1
     checksOk+=1
@@ -94,6 +161,16 @@ def resultado_ok(mensaje):
     print(f"    \033[92m[CORRECTO]:\033[0m {mensaje}")
 
 def leer_fichero(ruta, paso="General"):
+    """
+    Lee el contenido de un fichero y lo devuelve como string.
+    Si el fichero no existe o no se puede leer, devuelve None.
+
+    Args:
+        ruta (str): Ruta ABSOLUTA del fichero
+        paso (str): Identificador del paso para el log
+    Return:
+        str o None: Contenido del fichero, o None si hubo error
+    """
     try:
         with open(ruta,"r") as f:
             return f.read()
@@ -104,7 +181,12 @@ def leer_fichero(ruta, paso="General"):
         registrar_errores(paso, f"Sin permisos para leer {ruta}")
         return None
     
+# RESUMEN FINAL
 def mostrar_resumen():
+    """
+    Muestra un resumen de todas las verificaciones realizadas 
+    con el conteo de éxitos, fallos y advertencias.
+    """
     print()
     print("="*100)
     print("RESUMEN DE VERIFICACIÓN DE MÓDULO 1 - SEGURIDAD EN ACCESO AL HARDWARE")
@@ -134,29 +216,35 @@ def mostrar_resumen():
 #=========================================================================================================
 
 #=========================================================================================================
-#VERIFICACIONES
+# VERIFICACION PASO 1 - Protección del GRUB
+#=========================================================================================================
 
-#verificamos el paso 1
-# Leemos primero el archivo 40_custom
-# buscamos si tenemos superusers y password_pbkdf2
-# leemos el fichero de configuración del GRUB
-# buscamos si tenemos superusers y password_pbkdf2
-# ambos ficheros tienen que tener la configuración
 def verificar_paso1():
+    """
+    Verifica que GRUB está protegido con contraseña. Comprueba:
+    1. Que /etc/grub.d/40_custom contiene la directiva "set superusers"
+    2. Que /etc/grub.d/40_custom contiene un hash "password_pbkdf2"
+    3. Que /boot/grub/grub.cfg (generado) también incluye esas directivas
+       Esto se utiliza para confirmar que se ejecutó "update-grub" tras la configuración.
+    """
     print()
     print("="*100)
     print("[PASO 1]: Verificación de la protección del gestor de arranque GRUB.")
     print("="*100)
 
+    # Verificamos si el fichero 40_custom existe
     contenidoCustom=leer_fichero(GRUB_CUSTOM_FILE, paso="Paso 1")
     if contenidoCustom is None:
+        #Si el fichero no existe, GRUB no está protegido
         resultado_fail(
             f"No se encontró {GRUB_CUSTOM_FILE}. GRUB no tiene configuración de contraseña", 
             paso="Paso 1")
         return
     
-    #si el archivos existe
+    # Verificamos si la directiva "set superusers" está presente
+    #Esta directiva define qué clase de usuarios pueden editar las entradas de GRUB
     if "set superusers" in contenidoCustom:
+        #Extrae el nombre del superuser para mostrarlo
         for linea in contenidoCustom.splitlines():
             if "set superusers" in linea:
                 resultado_ok(f"Superusuario GRUB configurado: {linea.strip()}")
@@ -164,13 +252,16 @@ def verificar_paso1():
     else:
         resultado_fail(f"No se encontró la directiva 'set superusers' en {GRUB_CUSTOM_FILE}.", paso="Paso 1")
 
+    #Verifica si existe el fichero "password_pbkdf2", en el cual se encuentra el hash de la contraseña
     if "password_pbkdf2" in contenidoCustom:
         resultado_ok(f"Hash PBKDF2 de contraseña GRUB presente en {GRUB_CUSTOM_FILE}.")
     else:
         resultado_fail(f"No se encontró hash 'password_pbkdf2' en {GRUB_CUSTOM_FILE}.", paso="Paso 1")
 
     
-    #leemos el archivo de configuración y realizamos verificación cruzada
+    #Verificamos que "grub.cfg" está compilado e incluye la protección
+    #Después de ejecutar update-grub, la configuración de "40_custom" debe
+    #aparecer también en /boot/grub/grub.cfg 
     contenidoCfg=leer_fichero(GRUB_CFG_FILE, paso="Paso 1")
     if contenidoCfg is not None:
         if "superusers" in contenidoCfg and "password_pbkdf2" in contenidoCfg:
@@ -183,71 +274,99 @@ def verificar_paso1():
 
 
 
-#verificamos paso 2
-#comprobamos que el servicio está enmascarado
+#=========================================================================================================
+# VERIFICACIÓN PASO 2 - CTRL+ALT+DEL deshabilitado
+#=========================================================================================================
 
 def verificar_paso2():
+    """
+    Verifica que Ctrl+alt+del está deshabilitado. Comprueba:
+    1. Que ctrl-alt-del.target está enmascarado en systemd
+    2. Que el enlace simbólico apunta a /dev/null
+    """
     print()
     print("="*100)
     print("[PASO 2]")
     print("="*100)
 
+    #systemctl is-enabled devuelve uno de los siguientes estados: enabled, disabled, masked, static, etc
+    #Nos interesa sobre todo "masked" y "disabled".
     resultado=subprocess.run(["systemctl", "is-enabled", "ctrl-alt-del.target"], capture_output=True, text=True)
     estado=resultado.stdout.strip()
 
     if estado=="masked":
+        #"masked" significa que el target apunta a /dev/null y no puede activarse
         resultado_ok("ctrl-alt-del.target está enmascarado. La combinación de teclas está desactivada.")
     elif estado=="disabled":
+        #"disabled" significa que no se activa automáticamente, pero podría
+        #activarse manualmente por alguna aplicación u otros usuarios.
+        #No es tan seguro como "masked"
         resultado_warn("ctrl-alt-del.target está deshabilitado pero no enmascarado.")
         resultado_warn("Se recomienda enmascarar usando el siguiente comando: sudo systemctl mask ctrl-alt-del.target")
     else:
+        #Cualquier otro estado significa que no está enmascarado y por lo tanto sigue activo.
         resultado_fail(f"ctrl-alt-del.target tiene estado: '{estado}'.", paso="Paso 2")
         resultado_fail("Ctrl+alt+delete puede reiniciar el sistema.", paso="Paso 2")
 
-    #verificamos el estado para confirmar que está enmascarado
-
+    #Verificación adicional más detallada de systemctl
     resultado=subprocess.run(["systemctl", "status", "ctrl-alt-del.target"], capture_output=True, text=True)
     salida=resultado.stdout
 
+    #Busca la palabra "masked" en la salida de status
     if "masked" in salida.lower():
         resultado_ok("Verificación cruzada: systemctl status confirma masked.")
     else:
+        #Si "is-enabled" dice "masked" pero "status" no lo confirma, hay incosistencia.
         if estado=="masked":
             resultado_warn("Inconsistencia: is-enabled devuelve 'masked' pero status no lo confirma.")
 
 
-#verificamos paso 3
-#comprobamos que el fichero existe, las reglas están dentro del fichero y que el módulo de USB no está en memoria
+#=========================================================================================================
+# VERIFICACIÓN PASO 3 - Deshabilitar USB
+#=========================================================================================================
+
 def verificar_paso3():
+    """
+    Verifica que el almacenamiento USB está deshabilitado. Comprueba:
+    1. Que existe /etc/modprobe.d/usb-storage.conf con la regla blacklist
+    2. Que la regla "instal... /bin/false" también está presente
+    3. Que el módulo "usb_storage" NO está cargado en memoria.
+    """
     print()
     print("="*100)
     print("[PASO 3]: Almacenamiento USB deshabilitado")
     print("="*100)
 
+    #Verificamos si el fichero de configuración "modprobe" existe
     contenidoModprobe=leer_fichero(USB_MODPROBE_FILE, paso="Paso 3")
     if contenidoModprobe is None:
         resultado_fail(f"No se encontró {USB_MODPROBE_FILE}.", paso="Paso 3")
         resultado_fail("No hay regla de bloqueo para usb-storage.", paso="Paso 3")
     else:
+        #La directiva "blacklist usb-storage" impide la carga automática del módulo
         if "blacklist usb-storage" in contenidoModprobe:
             resultado_ok("Directiva 'blacklist usb-storage' presents.")
         else:
             resultado_fail("Falta la directiva'blacklist usb-storage'.", paso="Paso 3")
 
+        #La directiva "install usb-storage /bin/false" impide la carga manual del módulo
         if "install usb-storage /bin/false" in contenidoModprobe:
             resultado_ok("Directiva 'install usb-storage /bin/false' presente.")
         else:
             resultado_warn("Falta la directiva 'install usb-storage /bin/false'.")
             resultado_warn("El módulo podría cargarse manualmente con 'modprobe usb_storage'.")
     
+    # "lsmod" lista todos los módulos del kernel actualmente cargados
     resultado=subprocess.run(["lsmod"], capture_output=True, text=True)
     if "usb_storage" in resultado.stdout:
+        # Si el módulo está cargado, la regla no se ha aplicado correctamente
         resultado_fail("El módulo usb_storage está cargado en memoria.", paso="Paso 3")
         resultado_fail("Ejecuta: sudo modprobe -r usb_storage", paso="Paso 3")
     else:
+        #El módulo no está cargado, todo está correcto
         resultado_ok("El módulo usb_storage NO está cargado en memoria.")
 
-        
+    #Verificamos que las directivas en modprobe están activas
     resultado=subprocess.run(["modprobe", "--showconfig"], capture_output=True, text=True)
     configModprobe=resultado.stdout
     
@@ -264,11 +383,18 @@ def verificar_paso3():
 
 
   
-
 def main():
+    """
+    Función principal. Ejecuta todas las verificaciones en orden y muestra el resumen final.
+    """
+
+    #Verifica permisos de root
     comprobar_root()
+
+    #Inicializa el sistema de logging
     configurar_logging()
 
+    #Cabecera del script
     print()
     print("="*70)
     print("     VERIFICACIÓN: Módulo 1 - Seguridad en Acceso al Hardware - Ubuntu Server 24.04.4 LTS")
@@ -276,17 +402,23 @@ def main():
     print()
     print("         Comprobando configuraciones...")
 
-    verificar_paso1()
-    verificar_paso2()
-    verificar_paso3()
+    #Funciones para ejecutar las verificaciones en orden
+    verificar_paso1()   #GRUB protegido
+    verificar_paso2()   #Ctrl+Alt+Del deshabilitado
+    verificar_paso3()   #USB deshabilitado
 
+    #Mostrar resumen final con contadores
     mostrar_resumen()
 
+    #Devolver código de salida según resultado
+    #0 = todo bien, 1= hay fallos
     if checksFail>0:
         sys.exit(1)
     else:
         sys.exit(0)
 
-
+#=========================================================================================================
+# PUNTO DE ENTRADA
+#=========================================================================================================
 if __name__=="__main__":
     main()
