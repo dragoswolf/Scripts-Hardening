40072dc9772e346f3238324fd6622dadcebb9b23
Wolf <dragosgeorgestan.17@gmail.com>
2026-07-14 17:47:44 +0000

    Delete openscapAudit directory

--- openscapAudit/openscap_script.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-
-mod = input("Nombre del módulo: ").strip()
-fichero=f"openscap_{mod}.html"
-
-subprocess.run(["sudo", "oscap", "xccdf", "eval", "--report", fichero, "--profile", "xccdf_org.ssgproject.content_profile_cis_level1_server", "/usr/share/xml/scap/ssg/content/ssg-ubuntu2404-ds.xml"])
-
